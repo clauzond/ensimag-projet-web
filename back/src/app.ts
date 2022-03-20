03be70e67ebbecb08af51fe31790b5c65d8717ab
Damien C <thecsmine@gmail.com>
2022-03-20 17:30:17 +0100

    corriger le mauvais code d'aurélien ://

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,25 +1,34 @@
-import express from "express";
-import { utilisateur, histoire, paragraphe, historique } from "./model";
+import express from 'express';
+import { utilisateur, histoire, paragraphe, historique } from './model';
 
 const PORT = 8080;
 const app = express();
 
-const clauzond = await utilisateur.create({ id: "clauzondid", pwd: "clauzonmdp" });
+const clauzond = await utilisateur.create({
+	id: 'clauzondid',
+	pwd: 'clauzonmdp'
+});
 const paragrapheclauzond = await paragraphe.create({ contenu: 'woooow' });
 const histoireClauzond = await histoire.create({
-  idAuteur: clauzond.get("id"),
-  idParaInit: paragrapheclauzond.get("id")
+	titre: 'Histoire de clauzond (ya des dingueries)',
+	idAuteur: clauzond.get('id'),
+	idParaInit: paragrapheclauzond.get('id')
 });
 const histoClauzond = await historique.create({
-  idUtil: clauzond.get("id"),
-  idHist: histoireClauzond.get("id"),
-  idPara: [paragrapheclauzond.get("id")]
-})
+	idUtil: clauzond.get('id'),
+	idHist: histoireClauzond.get('id'),
+	idPara: [paragrapheclauzond.get('id')]
+});
 
 app.use((req, res) => {
-  res.send("Salut clauzond, ton id c'est" + clauzond.get("id") + " et ton historique c'est" + histoClauzond.get("idPara"));
+	res.send(
+		"Salut clauzond, ton id c'est" +
+			clauzond.get('id') +
+			" et ton historique c'est" +
+			histoClauzond.get('idPara')
+	);
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend up at http://localhost:${PORT}`);
+	console.log(`Backend up at http://localhost:${PORT}`);
 });
