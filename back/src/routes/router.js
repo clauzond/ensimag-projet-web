767987be45d879bb7d7fb54e21e48a596173e876
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-03-26 09:45:51 +0100

    doc: Add instructions on how to use api

--- back/src/routes/router.js
+++ back/src/routes/router.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import story from './histoire.js';
 import login from './login.js';
 const router = express.Router();
 
 router.use(story);
 router.use(login);
+router.use(function (req, res, next) {
+	res.send(`
+	Si ca marche pas faite npm run create-db  <br>
+	Creer un utilisateur: POST /api/register<br>
+	{ "username": "clauzond", "password": "clauzonmdp" }<br>
+	Se connecter: POST /api/login<br>
+	{ "username": "clauzond", "password": "clauzonmdp" }<br>
+	Renvoie le token jwt<br>
+	`);
+	next();
+});
 
 export { router };
