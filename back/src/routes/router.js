d7a4c4de723c95de3f22a9e1ec1714208337b613
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-03-30 21:22:04 +0200

    fix: Swagger working on '/'

--- back/src/routes/router.js
+++ back/src/routes/router.js
@@ -2,8 +2,14 @@ import express from 'express';
 import login from './login.js';
 import story from './histoire.js';
 import { auth } from '../util/middleware.js';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerFile from '../../swagger_output.json';
 
 const router = express.Router();
+
+// Swagger Documentation
+router.use('/', serve);
+router.get('/', setup(swaggerFile));
 
 // Login route (no auth required)
 router.use(login);
