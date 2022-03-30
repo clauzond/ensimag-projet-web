e96ae7daea64ea61cfcbbcf1f4522827ec704275
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-03-30 23:41:31 +0200

    test: json.stringify not needed
    send already stringifies the json

--- back/src/__tests__/api.login.test.js
+++ back/src/__tests__/api.login.test.js
@@ -10,12 +10,10 @@ describe('POST /api/register', () => {
 		response = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					name: username,
-					password: 'clauzondmdp'
-				})
-			);
+			.send({
+				name: username,
+				password: 'clauzondmdp'
+			});
 		expect(response.statusCode).toBe(status.BAD_REQUEST);
 		expect(response.body.message).toBe(
 			'You must specify the username and password'
@@ -24,12 +22,10 @@ describe('POST /api/register', () => {
 		response = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: '',
-					password: 'clauzondmdp'
-				})
-			);
+			.send({
+				username: '',
+				password: 'clauzondmdp'
+			});
 		expect(response.statusCode).toBe(status.BAD_REQUEST);
 		expect(response.body.message).toBe(
 			'Username or password must not be empty'
@@ -38,12 +34,10 @@ describe('POST /api/register', () => {
 		response = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: username,
-					password: '1234'
-				})
-			);
+			.send({
+				username: username,
+				password: '1234'
+			});
 		expect(response.statusCode).toBe(status.BAD_REQUEST);
 		expect(response.body.message).toBe(
 			'Password must be at least 6 characters'
@@ -52,13 +46,11 @@ describe('POST /api/register', () => {
 		response = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: username,
-					password:
-						'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam'
-				})
-			);
+			.send({
+				username: username,
+				password:
+					'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam'
+			});
 		expect(response.statusCode).toBe(status.BAD_REQUEST);
 		expect(response.body.message).toBe(
 			'Password must be less than 72 characters'
@@ -67,24 +59,20 @@ describe('POST /api/register', () => {
 		response = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: username,
-					password: 'clauzondmdp'
-				})
-			);
+			.send({
+				username: username,
+				password: 'clauzondmdp'
+			});
 		expect(response.statusCode).toBe(status.CREATED);
 		expect(response.body.message).toBe(`User ${username} was registered`);
 
 		let response1 = await request(app)
 			.post('/api/register')
 			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: username,
-					password: 'clauzondmdp'
-				})
-			);
+			.send({
+				username: username,
+				password: 'clauzondmdp'
+			});
 		expect(response1.statusCode).toBe(status.NOT_MODIFIED);
 	});
 });
