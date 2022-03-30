97278a0b00764493ca4ac9c2d9db7259c895e6ba
Damien C <thecsmine@gmail.com>
2022-03-30 19:46:58 +0200

    Update createDb.js

--- back/src/createDb.js
+++ back/src/createDb.js
@@ -1,4 +1,4 @@
 import { database } from './models/database.js';
 
-await database.sync({ force: true });
+database.sync({ force: true });
 console.log('All models were synchronized successfully.');
