39dbce9c04b447b1a8a7c838ed5fc7ec239be004
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-03-31 16:51:10 +0200

    fix: auth only certain routes

--- back/src/routes/histoire.js
+++ back/src/routes/histoire.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { story } from '../controllers/histoire.js';
+import { auth } from '../util/middleware.js';
 
 const router = express.Router();
-router.post('/api/histoire/', story.createStory);
+
 router.get('/api/histoire/:idHistoire', story.getStory);
+router.use(auth);
+router.post('/api/histoire/', story.createStory);
 router.put('/api/histoire/:idHistoire', story.updateStory);
 
 export default router;
