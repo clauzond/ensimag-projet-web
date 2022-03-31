9bf233cb75bcde889470b6b12e1a4218ae4f8f7a
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-03-31 12:40:56 +0200

    feat: paragraphe creation

--- back/src/controllers/paragraphe.js
+++ back/src/controllers/paragraphe.js
@@ -3,11 +3,12 @@ import { RequestError } from '../util/requestError.js';
 import status from 'http-status';
 import { Paragraphe } from '../models/index.js';
 import { checkStoryId } from './histoire.js';
+import { database } from '../models/database.js';
 
-async function checkParagrapheId(req) {
+async function checkParagraphId(req) {
 	if (!has(req.params, 'idParagraphe')) {
 		throw new RequestError(
-			'You must specified paragraphe id',
+			'You must specified paragraph id',
 			status.BAD_REQUEST
 		);
 	}
@@ -17,36 +18,58 @@ async function checkParagrapheId(req) {
 	// Get paragraphe object
 	const paragraphe = await Paragraphe.findByPk(paragrapheId);
 	if (!paragraphe) {
-		throw new RequestError(
-			"The paragraphe doesn't exist",
-			status.NOT_FOUND
-		);
+		throw new RequestError("The paragraph doesn't exist", status.NOT_FOUND);
 	}
 	return paragraphe;
 }
 
 export const paragraphe = {
-	async createParagraphe(req, res) {
-		const story = checkStoryId();
+	async createParagraph(req, res) {
+		const story = checkStoryId(req);
 
-		// Check if the user is a collaborator of the story
-		if (!(await story.isCollaborator(req.user))) {
+		if (!has(req.body, 'titreChoix')) {
 			throw new RequestError(
-				'You are not allowed to create a paragraphe'
+				'You must specified choice title',
+				status.BAD_REQUEST
 			);
 		}
 
-		res.json({ status: true, message: 'Returning user' });
+		// Check if the user is a collaborator of the story
+		if (!(await story.isCollaborator(req.user))) {
+			throw new RequestError('You are not allowed to create a paragraph');
+		}
+
+		// TODO: check if the user write only one paragraphe
+
+		// Add empty paragraphe
+		const paragraph = await Paragraphe.create({
+			contenu: null,
+			estVerrouille: false,
+			estConclusion: false
+		});
+
+		// Add choice
+		const choice = await addChoix({
+			titreChoix: req.body.titreChoix,
+			condition: has(req.body, 'condition') ? req.body.condition : null
+		});
+
+		res.json({
+			status: true,
+			message: 'Paragraph created',
+			paragraph: paragraph,
+			choice: choice
+		});
 	},
-	async updateParagraphe(req, res) {
+	async updateParagraph(req, res) {
 		//TODO
 		res.json({ status: true, message: 'Returning user' });
 	},
-	async deleteParagraphe(req, res) {
+	async deleteParagraph(req, res) {
 		//TODO
 		res.json({ status: true, message: 'Returning user' });
 	},
-	async getParagraphe(req, res) {
+	async getParagraph(req, res) {
 		//TODO
 		res.json({ status: true, message: 'Returning user' });
 	}
