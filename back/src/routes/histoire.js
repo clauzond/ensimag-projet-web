c1325b623d4f8e00e767ce7a2bf9b5ded663966e
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-02 11:09:05 +0200

    feat: add readonly paragraph

--- back/src/routes/histoire.js
+++ back/src/routes/histoire.js
@@ -3,7 +3,6 @@ import { story } from '../controllers/histoire.js';
 
 const router = express.Router();
 
-router.get('/api/readOnly/histoire', story.getStories);
 router.post('/api/histoire/', story.createStory);
 router.get('/api/histoire/:idHistoire', story.getStory);
 router.put('/api/histoire/:idHistoire', story.updateStory);
--- back/src/routes/readOnly.js
+++ back/src/routes/readOnly.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { story } from '../controllers/histoire.js';
+import { paragraphe } from '../controllers/paragraphe.js';
 
 const router = express.Router();
 
 router.get('/api/readOnly/histoire', story.getStories);
+router.get(
+	'/api/histoire/:idHistoire/paragraphe/:idParagraphe',
+	paragraphe.getParagraph
+);
 
 export default router;
