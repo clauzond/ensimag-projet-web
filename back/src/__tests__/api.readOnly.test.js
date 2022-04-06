50d43b2be8533ecd193ee35f11a34eea8893129b
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-06 10:36:31 +0200

    test: begin readOnly story

--- back/src/__tests__/api.readOnly.test.js
+++ back/src/__tests__/api.readOnly.test.js
@@ -1,21 +1,13 @@
 import { app } from '../app.js';
 import request from 'supertest';
 import status from 'http-status';
-const username = 'clauzond_login';
-const password = 'clauzondmdp_login';
+import { createStory } from './util/setupDb.js';
 
-describe('POST /api/register', () => {
-	test('Test of valid registration', async () => {
-		const response = await request(app)
-			.post('/api/register')
-			.set('Content-Type', 'application/json')
-			.send(
-				JSON.stringify({
-					username: username,
-					password: password
-				})
-			);
-		expect(response.statusCode).toBe(status.CREATED);
-		expect(response.body.message).toBe(`User ${username} was registered`);
+describe('GET /api/readOnly/histoire', () => {
+	const story = createStory("L'histoire de clauzond", true, true);
+
+	test('Test of get a public story', async () => {
+		const response = await request(app).get('/api/readOnly/histoire');
+		expect(response.statusCode).toBe(status.OK);
 	});
 });
