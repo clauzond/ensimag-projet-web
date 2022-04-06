5f1d6d012ae3ef0c08821a1150cdd6f92e55f39d
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-06 22:11:06 +0200

    feat: welcome view

--- front/src/views/Welcome.js
+++ front/src/views/Welcome.js
@@ -8,42 +8,42 @@ import {
 	Icon,
 	Box,
 	Button,
+	VStack,
+	FormControl,
+	Input,
+	ScrollView,
 } from 'native-base';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function Welcome() {
 	return (
 		<View>
-			<Center>
-				<Flex
-					h="100%"
-					alignItems="center"
-					justifyContent="space-between"
-					py="40px"
-				>
-					<Flex alignItems="center">
-						<Icon
-							as={MaterialCommunityIcons}
-							name="book"
-							color="coolGray.800"
-							size="2xl"
-							_dark={{
-								color: 'warmGray.50',
-							}}
-						/>
-						<Heading color="emerald.400">Herobook</Heading>
-					</Flex>
-					<Box>
-						<Button onPress={() => console.log('hello world')}>
-							S'inscrire
-						</Button>
-						<Button>Se connecter</Button>
-					</Box>
-					<Box>
-						<Text>Continuer en tant qu'invité</Text>
-					</Box>
+			<Flex h="100%" justify="space-between" align="center" py={4}>
+				<Box />
+				<Flex alignItems="center">
+					<Icon
+						as={MaterialCommunityIcons}
+						name="book"
+						color="coolGray.800"
+						size="2xl"
+						_dark={{
+							color: 'warmGray.50',
+						}}
+					/>
+					<Heading fontSize="4xl" color="primary.400">
+						Mon App
+					</Heading>
 				</Flex>
-			</Center>
+				<Box p="2" w="90%" py="8" colorScheme="secondary">
+					<Button mt="2">Inscription</Button>
+					<Button mt="2" colorScheme="secondary">
+						Connexion
+					</Button>
+					<Text mt={2} textAlign="center">
+						Continuer en tant qu'invité
+					</Text>
+				</Box>
+			</Flex>
 		</View>
 	);
 }
