77a877a3415403ca9773911b3f3c09910340c9b2
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-06 22:51:26 +0200

    fix: apply prettier on files

--- front/src/index.js
+++ front/src/index.js
@@ -2,11 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import packageInfo from './app.json';
 
 AppRegistry.registerComponent(packageInfo.name, () => App);
 AppRegistry.runApplication(packageInfo.name, {
-  rootTag: document.getElementById('root'),
+	rootTag: document.getElementById('root'),
 });
