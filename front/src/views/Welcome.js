15b54b10619c0737e626d88f050bb7a6da9d1d88
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-06 18:04:59 +0200

    view(welcome): very very basic welcome screen

--- front/src/views/Welcome.js
+++ front/src/views/Welcome.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Heading, View, Text, Center, Icon, Box } from 'native-base';
+import {
+	Flex,
+	Heading,
+	View,
+	Text,
+	Center,
+	Icon,
+	Box,
+	Button,
+} from 'native-base';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function Welcome() {
 	return (
 		<View>
 			<Center>
-				<Heading>
-					<Box>
+				<Flex
+					h="100%"
+					alignItems="center"
+					justifyContent="space-between"
+					py="40px"
+				>
+					<Flex alignItems="center">
 						<Icon
 							as={MaterialCommunityIcons}
 							name="book"
@@ -17,15 +31,18 @@ export function Welcome() {
 								color: 'warmGray.50',
 							}}
 						/>
+						<Heading color="emerald.400">Herobook</Heading>
+					</Flex>
+					<Box>
+						<Button onPress={() => console.log('hello world')}>
+							S'inscrire
+						</Button>
+						<Button>Se connecter</Button>
 					</Box>
-					A component library for the{' '}
-					<Heading color="emerald.400">React Ecosystem</Heading>
-				</Heading>
-				<Text pt="3" fontWeight="md">
-					NativeBase is a simple, modular and accessible component
-					library that gives you building blocks to build you React
-					applications.
-				</Text>
+					<Box>
+						<Text>Continuer en tant qu'invité</Text>
+					</Box>
+				</Flex>
 			</Center>
 		</View>
 	);
