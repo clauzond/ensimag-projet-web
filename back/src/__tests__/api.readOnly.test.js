c1fee47abe5526912f1f49eb764b791ae6219361
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-08 22:28:09 +0200

    test: GET public story by id

--- back/src/__tests__/api.readOnly.test.js
+++ back/src/__tests__/api.readOnly.test.js
@@ -52,13 +52,13 @@ describe('GET /api/readOnly/histoire', () => {
 		const publicStoriesNb = response.body.stories.length;
 
 		// Get and check the private story is not in the public stories list
-		const publicStory = response.body.stories[publicStoriesNb - 1];
-		expect(publicStory.titre).not.toBe(storyTitle);
-		expect(publicStory.idAuteur).not.toBe(username);
-		expect(publicStory.ParagrapheInitial.contenu).not.toBe(
+		const privateStory = response.body.stories[publicStoriesNb - 1];
+		expect(privateStory.titre).not.toBe(storyTitle);
+		expect(privateStory.idAuteur).not.toBe(username);
+		expect(privateStory.ParagrapheInitial.contenu).not.toBe(
 			firstParagraphContent
 		);
-		expect(publicStory.ParagrapheInitial.idRedacteur).not.toBe(username);
+		expect(privateStory.ParagrapheInitial.idRedacteur).not.toBe(username);
 	});
 
 	test('Test of get a public story that not lead to a conclusion', async () => {
@@ -114,6 +114,96 @@ describe('GET /api/readOnly/histoire', () => {
 			firstParagraphContent
 		);
 		expect(publicStory.ParagrapheInitial.idRedacteur).not.toBe(username);
+	});
+});
+
+describe('GET /api/readOnly/histoire/:idHistoire', () => {
+	test('Test of get a unique public story', async () => {
+		// Init a public story with a valid paragraph (with content and that lead to conclusion)
+		const username = 'clauzond';
+		const storyTitle = "L'histoire publique et unique de clauzond";
+		const firstParagraphContent = 'Ceci est le paragraphe de clauzond';
+		const newStory = await createStory(
+			storyTitle,
+			username,
+			true,
+			firstParagraphContent,
+			true
+		);
+
+		const response = await request(app).get(
+			`/api/readOnly/histoire/${newStory.id}`
+		);
+		expect(response.statusCode).toBe(status.OK);
+
+		// Get and check the public story just being import
+		const publicStory = response.body.story;
+		expect(publicStory.id).toBe(newStory.id);
+		expect(publicStory.titre).toBe(storyTitle);
+		expect(publicStory.idAuteur).toBe(username);
+		expect(publicStory.estPublique).toBe(true);
+	});
+
+	test('Test of get a unique private story', async () => {
+		// Init a public story with a valid paragraph (with content and that lead to conclusion)
+		const username = 'clauzond_private';
+		const storyTitle = "L'histoire privée et unique de clauzond";
+		const firstParagraphContent =
+			'Ceci est le paragraphe de clauzond privé';
+		const newStory = await createStory(
+			storyTitle,
+			username,
+			false,
+			firstParagraphContent,
+			true
+		);
+
+		const response = await request(app).get(
+			`/api/readOnly/histoire/${newStory.id}`
+		);
+		expect(response.statusCode).toBe(status.FORBIDDEN);
+		expect(response.body.message).toBe('This story is private');
+	});
+
+	test('Test of get a unique public story that not lead to a conclusion', async () => {
+		// Init a public story with a valid paragraph (with content and that lead to conclusion)
+		const username = 'clauzond_sans_conclusion';
+		const storyTitle = "L'histoire publique de clauzond sans conclusion";
+		const firstParagraphContent =
+			'Ceci est le paragraphe de clauzond sans conclusion';
+		const newStory = await createStory(
+			storyTitle,
+			username,
+			true,
+			firstParagraphContent,
+			false
+		);
+
+		const response = await request(app).get(
+			`/api/readOnly/histoire/${newStory.id}`
+		);
+		expect(response.statusCode).toBe(status.FORBIDDEN);
+		expect(response.body.message).toBe('This story is private');
+	});
+
+	test('Test of get a unique public story where the init paragraph is null', async () => {
+		// Init a public story with a valid paragraph (with content and that lead to conclusion)
+		const username = 'clauzond_sans_paragraphe';
+		const storyTitle = "L'histoire publique de clauzond sans paragraphe";
+		const firstParagraphContent = null;
+		const newStory = await createStory(
+			storyTitle,
+			username,
+			true,
+			firstParagraphContent,
+			true
+		);
+
+		const response = await request(app).get(
+			`/api/readOnly/histoire/${newStory.id}`
+		);
+		expect(response.statusCode).toBe(status.FORBIDDEN);
+		expect(response.body.message).toBe('This story is private');
 	});
 });
 
