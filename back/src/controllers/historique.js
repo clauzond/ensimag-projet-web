722bba4bda7cbb548a8ab5e797a5358edc1caea1
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-08 22:49:41 +0200

    feat: get, remove, clear history controller

--- back/src/controllers/historique.js
+++ back/src/controllers/historique.js
@@ -2,6 +2,7 @@ import has from 'has-keys';
 import { RequestError } from '../util/requestError.js';
 import status from 'http-status';
 import { checkStoryId } from './histoire.js';
+import { checkParagraphId } from './paragraphe.js';
 
 export const history = {
 	async saveHistory(req, res) {
@@ -22,6 +23,35 @@ export const history = {
 		});
 	},
 	async getHistory(req, res) {
-		// TODO
+		const story = await checkStoryId(req);
+
+		const history = await req.user.getHistorique(story);
+
+		res.json({
+			status: true,
+			message: 'Returning history',
+			history: history
+		});
+	},
+	async removeHistory(req, res) {
+		const story = await checkStoryId(req);
+		const paragraph = await checkParagraphId(req);
+
+		await req.user.removeHistorique(story, paragraph);
+
+		res.json({
+			status: true,
+			message: 'History removed'
+		});
+	},
+	async clearHistory(req, res) {
+		const story = await checkStoryId(req);
+
+		await req.user.clearHistorique(story);
+
+		res.json({
+			status: true,
+			message: 'History cleared'
+		});
 	}
 };
--- back/src/routes/historique.js
+++ back/src/routes/historique.js
@@ -3,7 +3,12 @@ import { history } from '../controllers/historique.js';
 
 const router = express.Router();
 
-router.get('/api/historique/', history.getHistory);
-router.post('/api/historique/', history.saveHistory);
+router.get('/api/historique/:idHistoire', history.getHistory);
+router.post('/api/historique/:idHistoire', history.saveHistory);
+router.delete('/api/historique/:idHistoire', history.clearHistory);
+router.delete(
+	'/api/historique/:idHistoire/paragraphe/:idParagraphe',
+	history.removeHistory
+);
 
 export default router;
