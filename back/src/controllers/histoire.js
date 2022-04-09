b02c7a7d499c6cf19aa95352d62c8c069336a493
Damien C <thecsmine@gmail.com>
2022-04-09 16:57:21 +0200

    feat: (add/remove)Collaborator

--- back/src/controllers/histoire.js
+++ back/src/controllers/histoire.js
@@ -1,7 +1,7 @@
 import has from 'has-keys';
 import status from 'http-status';
 import { Op } from 'sequelize';
-import { Histoire, Paragraphe } from '../models/index.js';
+import { Utilisateur, Histoire, Paragraphe } from '../models/index.js';
 import { RequestError } from '../util/requestError.js';
 
 async function checkStoryId(req) {
@@ -165,7 +165,7 @@ export const story = {
 		// Check if the user is the author
 		if (!(await story.isAuthor(req.user))) {
 			throw new RequestError(
-				'You are not allowed to modified this story',
+				'You are not allowed to modify this story',
 				status.FORBIDDEN
 			);
 		}
@@ -173,7 +173,7 @@ export const story = {
 		// Throws error if the params are not correct
 		if (!has(req.body, 'estOuverte') && !has(req.body, 'estPublique')) {
 			throw new RequestError(
-				'You must specified estPublique or estOuverte params',
+				'You must specify estPublique or estOuverte params',
 				status.BAD_REQUEST
 			);
 		}
@@ -206,6 +206,86 @@ export const story = {
 		const story = await checkStoryId(req);
 		await checkIsPrivateStory(story);
 		res.json({ status: true, message: 'Returning story', story: story });
+	},
+	async addCollaborator(req, res) {
+		// Get story object
+		const story = await checkStoryId(req);
+
+		// Only the story author can add collaborators
+		if (!(await story.isAuthor(req.user))) {
+			throw new RequestError(
+				'Only the author of the story can add or remove collaborators',
+				status.FORBIDDEN
+			);
+		}
+
+		if (!has(req.body, "idCollaborateur")) {
+			throw new RequestError(
+				'You must specify idCollaborateur param',
+				status.BAD_REQUEST
+			);
+		}
+
+		// Verify that collaborator exists in database
+		const collaborator = await Utilisateur.findByPk(req.body.idCollaborateur);
+		if (collaborator === null) {
+			throw new RequestError(
+				'User specified in idCollaborateur was not found',
+				status.BAD_REQUEST
+			);
+		}
+
+		// Verify that collaborator is not already added
+		if (await story.hasCollaborateur(collaborator)) {
+			throw new RequestError(
+				'User specified in idCollaborateur is already a collaborator of this story',
+				status.NOT_MODIFIED
+			);
+		}
+
+		await story.addCollaborateur(collaborator);
+
+		res.json({ status: true, message: 'User added as collaborator of the story', story: story, collaborator: collaborator });
+	},
+	async removeCollaborator(req, res) {
+		// Get story object
+		const story = await checkStoryId(req);
+
+		// Only the story author can add collaborators
+		if (!(await story.isAuthor(req.user))) {
+			throw new RequestError(
+				'Only the author of the story can add or remove collaborators',
+				status.FORBIDDEN
+			);
+		}
+
+		if (!has(req.body, "idCollaborateur")) {
+			throw new RequestError(
+				'You must specify idCollaborateur param',
+				status.BAD_REQUEST
+			);
+		}
+
+		// Verify that collaborator exists in database
+		const collaborator = await Utilisateur.findByPk(req.body.idCollaborateur);
+		if (collaborator === null) {
+			throw new RequestError(
+				'User specified in idCollaborateur was not found',
+				status.BAD_REQUEST
+			);
+		}
+
+		// Verify that user is already a collaborator
+		if (!(await story.hasCollaborateur(collaborator))) {
+			throw new RequestError(
+				'User specified in idCollaborateur is not a collaborator of this story',
+				status.NOT_MODIFIED
+			);
+		}
+
+		await story.removeCollaborator(collaborator);
+
+		res.json({ status: true, message: 'Successfully removed collaborator from story', story: story });
 	}
 };
 
