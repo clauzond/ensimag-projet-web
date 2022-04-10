2e03ee4ed67bd75f697f16a623a16ae46a721541
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-10 12:58:57 +0200

    fix: fix eslint errors on frontend

--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,36 +1,32 @@
 import { useAppStateContext } from '../contexts/AppState';
 import axios from 'axios';
-import { Text, View } from 'native-base';
+import { Text, Center } from 'native-base';
 import React from 'react';
 import { BACKEND } from '../globals';
 
 export function Home() {
-	const { token } = useAppStateContext();
-	const [username, setUsername] = React.useState('');
-	console.log(token);
+  const { token } = useAppStateContext();
+  const [username, setUsername] = React.useState('');
+  console.log(token);
 
-	const load = async () => {
-		try {
-			const response = await axios.get(`${BACKEND}/api/whoami`, {
-				headers: { 'x-access-token': token },
-			});
-			setUsername(response.data.data);
-		} catch (e) {
-			// ignore error, not connected
-		}
-	};
-	React.useEffect(() => {
-		load();
-	}, []);
+  const load = async () => {
+    try {
+      const response = await axios.get(`${BACKEND}/api/whoami`, {
+        headers: { 'x-access-token': token },
+      });
+      setUsername(response.data.data);
+    } catch (e) {
+      // ignore error, not connected
+    }
+  };
+  React.useEffect(() => {
+    load();
+  });
 
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			{username === '' ? (
-				<Text bold>Not connected</Text>
-			) : (
-				<Text>Connected as {username}</Text>
-			)}
-			<Text>got token: {token}</Text>
-		</View>
-	);
+  return (
+    <Center>
+      {username === '' ? <Text bold>Not connected</Text> : <Text>Connected as {username}</Text>}
+      <Text>got token: {token}</Text>
+    </Center>
+  );
 }
