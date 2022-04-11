4663e41022dc42c3d7bca512a9cb90e2429b2a85
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-11 22:40:54 +0200

    feat: display list of stories (public and authentified)

--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,34 +1,75 @@
 import { useAppStateContext } from '../contexts/AppState';
-import { Text, Center } from 'native-base';
-import React from 'react';
+import { Text, Center, StatusBar } from 'native-base';
+import React, { useState } from 'react';
 import { users } from '../services/users';
 import { history } from '../services/history';
+import { FlatList, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
 
 export function Home() {
   const { token } = useAppStateContext();
   const [username, setUsername] = React.useState('');
   const [stories, setStories] = React.useState('');
-  console.log(token);
+  const [selectedId, setSelectedId] = useState(null);
 
   const load = async () => {
-    const usernameFromApi = await users.whoami(token);
-    setUsername(usernameFromApi);
-    const storiesFromApi = await history.getPublicAuthentifiedStories(token);
-    setStories(storiesFromApi);
-    console.log("stories: "+ stories);
+    if (token !== '') {
+      // Authentified user case
+      const usernameFromApi = await users.whoami(token);
+      setUsername(usernameFromApi);
+      const storiesFromApi = await history.getPublicAuthentifiedStories(token);
+      setStories(storiesFromApi);
+    } else {
+      // Guest user case
+      const storiesFromApi = await history.getPublicStories();
+      setStories(storiesFromApi);
+    }
   };
 
   React.useEffect(() => {
     load();
   }, []);
 
-  // console.log(stories);
+  // List style
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+  });
+
+  const Item = ({ item, onPress, backgroundColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+      <Text>{item.titre}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.titre)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
 
   return (
-    <Center>
-      {username === '' ? <Text bold>Not connected</Text> : <Text>Connected as {username}</Text>}
-      <Text>got token: {token}</Text>
-      <Text>stories: {stories.length}</Text>
-    </Center>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={stories}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
   );
 }
