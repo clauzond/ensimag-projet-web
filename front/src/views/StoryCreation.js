5cdc30ab88ac40f5f1f6857786a7c241dc4637d1
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-04-13 19:07:55 +0200

    feat: continue story creation page

--- front/src/views/StoryCreation.js
+++ front/src/views/StoryCreation.js
@@ -1,45 +1,19 @@
-import { useAppStateContext } from '../contexts/AppState';
-import { Text, TextArea, Heading, StatusBar, HStack, Checkbox } from 'native-base';
+import { Text, TextArea, Heading, StatusBar, HStack, Checkbox, Button } from 'native-base';
 import React from 'react';
-import { users } from '../services/users';
-import { storyService } from '../services/story';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import Popup from 'react-native-easypopup';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 
 export function StoryCreation({ navigation }) {
-  const { token, setHistory } = useAppStateContext();
-  const [stories, setStories] = React.useState('');
-  const [selectedId, setSelectedId] = React.useState(null);
-
-  const [state, setState] = React.useState(false);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const load = async () => {
-    if (token !== '') {
-      // Authentified user case
-      const storiesFromApi = await storyService.getPublicAuthentifiedStories(token);
-      setStories(storiesFromApi);
-    } else {
-      // Guest user case
-      const storiesFromApi = await storyService.getPublicStories();
-      setStories(storiesFromApi);
-    }
-  };
-
-  const header = async () => {
-    if (token !== '') {
-      const username = await users.whoami(token);
-      // Set header buttons
-      navigation.setOptions({
-        title: `Story creation - ${username}`,
-      });
-    }
+    navigation.setOptions({
+      title: 'Create a story',
+    });
   };
 
   React.useEffect(() => {
-    load().then(_ => header());
+    load();
   }, []);
 
   // List style
@@ -47,19 +21,24 @@ export function StoryCreation({ navigation }) {
     container: {
       flex: 1,
       marginTop: StatusBar.currentHeight || 0,
+      margin: 10,
+      paddingTop: 5,
     },
-    item: {
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
+    input: {
+      height: 40,
+      width: 300,
+      borderBottomWidth: 1,
+      marginBottom: 10,
+      padding: 10,
     },
-    add_button: {
-      position: 'absolute',
-      bottom: 10,
-      right: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 50,
+    multiLinesInput: {
+      width: 300,
+      borderBottomWidth: 1,
+      marginBottom: 10,
+      padding: 10,
+    },
+    create: {
+      width: 125,
     },
   });
 
@@ -72,23 +51,14 @@ export function StoryCreation({ navigation }) {
   };
 
   return (
-    // view
     <SafeAreaView style={styles.container}>
-      <Heading>I'm a Heading</Heading>
-      <TextArea
-        shadow={2}
-        h={20}
-        placeholder="First paragraph"
-        w="200"
-        _light={{
-          placeholderTextColor: 'trueGray.700',
-        }}
-        _dark={{
-          bg: 'coolGray.800',
-        }}
-        _hover={{
-          bg: 'coolGray.200',
-        }}
+      <TextInput style={styles.input} placeholder="Story title" maxLength={255} />
+      <TextInput
+        style={styles.multiLinesInput}
+        placeholder="Content of the first paragraph"
+        maxLength={255}
+        multiline
+        numberOfLines={5}
       />
 
       <SegmentedControlTab
@@ -103,6 +73,9 @@ export function StoryCreation({ navigation }) {
           Make story public
         </Checkbox>
       </HStack>
+      <Button colorScheme="primary" style={styles.create}>
+        Create story
+      </Button>
     </SafeAreaView>
   );
 }
