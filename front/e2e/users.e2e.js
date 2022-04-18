f25d2208c0b90d3be0b5101e5fbd6c2af510aeb1
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-18 22:13:42 +0200

    fix(front): pass frontend test for ci

--- front/e2e/users.e2e.js
+++ front/e2e/users.e2e.js
@@ -2,16 +2,19 @@
 const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6InRlc3R1c2VyIn0.56yrdVOXpeD7CKWq_hDlLFJH2jfOHHCmfchyKA8PaaA';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 describe('User login signup test', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
 
   beforeEach(async () => {
+    // to avoid auto login with stored token from previous test, clear storage
     await device.reloadReactNative();
   });
 
-  it('should try to signup & login', async () => {
+  it('should signup', async () => {
     await element(by.text('Sign up')).tap();
     await element(by.id('username')).typeText('');
     await element(by.id('username')).tapReturnKey();
@@ -42,24 +45,43 @@ describe('User login signup test', () => {
     await element(by.id('password')).tapReturnKey();
 
     await element(by.text('Register')).tap();
+  });
 
-    // Register done, go back and login
+  it('should login', async () => {
+    // HACK: we are already connected, go back
+    // We should mock AsyncStorage instead, but I didn't get it working yet
+    await expect(element(by.text('Home'))).toBeVisible();
     await device.pressBack();
-    await element(by.text('Back')).tap();
+
     await element(by.text('Login')).tap();
 
-    // Login test
+    await element(by.id('username')).typeText('');
+    await element(by.id('username')).tapReturnKey();
+
+    await expect(element(by.text('You must specify an username'))).toBeVisible();
+
     await element(by.id('username')).typeText('testuser');
     await element(by.id('username')).tapReturnKey();
-    await element(by.id('password')).typeText('123456');
+
+    await element(by.id('password')).typeText('');
     await element(by.id('password')).tapReturnKey();
+
+    await expect(element(by.text('You must specify a password'))).toBeVisible();
+
+    await element(by.id('password')).typeText('a');
+    await element(by.id('password')).tapReturnKey();
+
+    await expect(element(by.text('Password must be at least 6 characters'))).toBeVisible();
+
+    await element(by.id('password')).replaceText('123456');
+    await element(by.id('password')).tapReturnKey();
+
     await element(by.text('Login')).tap();
+  });
 
-    // Go back
+  it('should login as guest', async () => {
+    await expect(element(by.text('Home'))).toBeVisible();
     await device.pressBack();
-    await element(by.text('Back')).tap();
-
-    // Continue as guest (should have no user)
     await element(by.text('Continue as guest')).tap();
   });
 });
