f6e00fa894e2cbad7d88c9546ef236d2e4fca559
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-28 00:15:27 +0200

    test(front): add more paragraph test (not working because of toast)

--- front/e2e/05-paragraphs.e2e.js
+++ front/e2e/05-paragraphs.e2e.js
@@ -2,8 +2,9 @@ import { connectUser, createStory, registerUser, createParagraph } from './util'
 
 const username = 'ParagraphsUser';
 const titleSimpleStory = 'firstStory';
+const titleSecondStory = 'secondStory';
 
-describe('User stories page test', () => {
+describe('Create stories tests', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -33,7 +34,36 @@ describe('User stories page test', () => {
     await expect(element(by.text('myContent\nGo to end\nEnd of story'))).toBeVisible();
   });
 
-  // TODO: move into function & add more complicated stories
-  // do: publish a story, with some paragraphs with no conclusion => do not display
-  // do: conditional paragraph, and reading test based on history (do not display if condition not set)
+  it('should not show a story, as long as a story branch is not finished', async () => {
+    await connectUser(username, true);
+
+    await createStory(titleSecondStory, 'content1', true);
+
+    await element(by.text(titleSecondStory)).tap();
+    await element(by.text('Set paragraphs')).tap();
+
+    await createParagraph('title2', 'content2', titleSecondStory);
+
+    await expect(element(by.text('title2'))).toBeVisible();
+    await createParagraph('title3', 'content3', titleSecondStory, { isConclusion: true });
+
+    await device.pressBack();
+    await device.pressBack();
+    await element(by.id('refresh')).tap();
+    await element(by.text(titleSecondStory)).tap();
+
+    // Paragraph title2 in unfinished story branch -> not display
+    await expect(
+      element(
+        by.text(`content1
+title3
+content3`)
+      )
+    ).toBeVisible();
+  });
+
+  // TODO: add choice into existing story:
+  // * test case pick parent pargraph
+  // * test case pick parent & child paragraph
+  // TODO: conditional paragraph, and reading test based on history (do not display if condition not set)
 });
